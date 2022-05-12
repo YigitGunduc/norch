f034d901553af009a5cef3fb3ec01bac107a7d60
Yigit Gunduc <ygunduc@gmail.com>
2022-05-12 20:56:19 +0300

    load weights and apply forward function

--- torchscript.js
+++ torchscript.js
@@ -1,45 +1,56 @@
-const math = require('math');
+'use strict';
+const fs = require('fs');
 
+function load_weights(path) {
+  let model_params = fs.readFileSync(path);
+  model_params = JSON.parse(model_params);
 
-// utils
-function splitArray(array, part) {
-    var tmp = [];
-    for(var i = 0; i < array.length; i += part) {
-        tmp.push(array.slice(i, i + part));
+  return model_params;
+}
+
+function matmul(a, b) {
+  var aNumRows = a.length, aNumCols = a[0].length,
+  bNumRows = b.length, bNumCols = b[0].length,
+  m = new Array(aNumRows);  // initialize array of rows
+  for (var r = 0; r < aNumRows; ++r) {
+    m[r] = new Array(bNumCols); // initialize the current row
+    for (var c = 0; c < bNumCols; ++c) {
+      m[r][c] = 0;             // initialize the current cell
+      for (var i = 0; i < aNumCols; ++i) {
+        m[r][c] += a[r][i] * b[i][c];
+      }
     }
-    return tmp;
+  }
+  return m;
 }
 
-function dot(A, B) {
-    var result = new Array(A.length).fill(0).map(row => new Array(B[0].length).fill(0));
+function transpose(m) {
+  return m[0].map((x, i) => m.map(x => x[i]));
+} 
 
-    return result.map((row, i) => {
-        return row.map((val, j) => {
-            return A[i].reduce((sum, elm, k) => sum + (elm*B[k][j]) ,0)
-        })
-    })
+function addmat(m, v) {
+  for (let i = 0; i < m[0].length; i++) {
+    m[0][i] = m[0][i] + v[i];
+  }
+  return m;
 }
 
+function forward(x, model_params) {
+  const weights = (model_params['weights']);
+  const biases = (model_params['biases']);
 
-transpose = m => m[0].map((x,i) => m.map(x => x[i]))
-
-const weights = [splitArray(Array(15).fill(1).map(x=>Math.random()), 3),
-                 splitArray(Array(15).fill(1).map(x=>Math.random()), 5),
-                 splitArray(Array(5).fill(1).map(x=>Math.random()), 1),
-                ];
-                 //Array(5).fill(1).map(x=>Math.random()),
-
-let input = [[1.2, 3.4, 1.4]];
-
-function forward(x, weights) {
-  for (let i = 0; i < weights.length - 1; i++) {
-    x = dot(x, transpose(weights[i]))
-    
+  for (let i = 0; i < weights.length; i++) {
+    x = matmul(x, transpose(weights[i]));
+    x = addmat(x, biases[i])
   }
 
-  return x[0]
+  return x[0];
 }
 
-out = forward(input, weights)
+const model_params = load_weights('weights.json');
+
+let input = [[1, 1, 1]];
+
+let out = forward(input, model_params)
 
 console.log(out);
