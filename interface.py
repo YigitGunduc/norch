dcc0a0aec0361dd20897e8ff1e23a2439285b250
Yigit Gunduc <ygunduc@gmail.com>
2022-05-12 20:55:55 +0300

    can save weights and biases to a json file

--- interface.py
+++ interface.py
@@ -1,33 +1,67 @@
 import torch
+import json
+import random
+import numpy as np
+import torch.nn.functional as F
 from torch import nn
 
 class MLP(nn.Module):
     # define model elements
     def __init__(self, n_inputs):
         super(MLP, self).__init__()
-        self.layer = nn.Linear(n_inputs, 1)
-        self.activation = nn.Sigmoid()
+        self.l = nn.Linear(3, 5)
+        self.l2 = nn.Linear(5, 3)
+        self.l3 = nn.Linear(3, 1)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
 
     # forward propagate input
     def forward(self, x):
-        x = self.layer(x)
-        x = self.activation(x)
+        x = self.l(x)
+        x = self.relu(x)
+        x = self.l2(x)
+        x = self.relu(x)
+        x = self.l3(x)
+        x = self.sigmoid(x)
         return x
 
 mlp = MLP(5)
 
+def save_weights(model, filename='weights.json'):
+    weights = []
+    biases = []
+    for layer in model.children():
+        if isinstance(layer, nn.Linear):
+            weights.append(layer.state_dict()['weight'].detach().numpy().tolist())
+            biases.append(layer.state_dict()['bias'].detach().numpy().tolist())
 
-x = (torch.nn.ParameterList(mlp.parameters()))
-def get_params(params):
-  out = []
-  
-  params = (torch.nn.ParameterList(mlp.parameters()))
-  for param in params:
-    out.append(param.detach().numpy())
 
-  return out[0]
+    model_params = {'weights': weights, 
+                    'biases': biases}
+
+    with open(filename, "w") as outfile:
+        json.dump(model_params, outfile)
     
-  
-print(get_params(mlp.parameters()))
+    return weights, biases
 
+  
+def relu(x):
+    return np.maximum(0, x)
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+inp = [1, 2, 3]
+x = torch.ones(3)
+print(mlp.forward(x))
+
+weights, biases = save_weights(mlp)
+
+x = x.numpy()
+for weight, bias in zip(weights, biases):
+    x = np.matmul(x, np.array(weight).T) + bias
+    # x = relu(x)
+
+# x = sigmoid(x)
+print(x)
 print('done')
